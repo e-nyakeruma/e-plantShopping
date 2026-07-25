83be3ef6f1b9ec4f9dd87b0bcc1f2eafe115c142
Edward Isaboke <edward.isaboke@gmail.com>
2026-07-25 09:29:36 -0400

    Update cart layout

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
+import { useDispatch } from 'react-redux';
 
 
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -300,7 +303,7 @@ function ProductList({ onHomeClick }) {
                                         />
                                         <div className="product-title">{plant.name}</div>
                                         <div className="product-description">{plant.description}</div>
-                                        <div className="product-cost">${plant.cost}</div>
+                                        <div className="product-cost">{plant.cost}</div>
                                         <button
                                             className="product-button"
                                             onClick={() => handleAddToCart(plant)}
